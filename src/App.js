8df24ad0288fc285e2f9874e467bb8694f2b89ab
sreenu9165 <sreenukorukolla@gmail.com>
2026-01-29 08:38:02 +0530

    Add Class 5 option to grade selection dropdown

--- src/App.js
+++ src/App.js
@@ -63,6 +63,7 @@ function App() {
             <label>Grade</label>
             <select name="grade" value={formData.grade} onChange={handleChange}>
               <option value="">Select grade</option>
+              <option value="Class 5">Class 5</option>
               <option value="Class 6">Class 6</option>
               <option value="Class 7">Class 7</option>
               <option value="Class 8">Class 8</option>
